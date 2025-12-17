447f502408afaf8d3e75efdd24a93dde85339bc0
Hdt91007 <hdt91007@gmail.com>
2025-12-17 11:51:12 -0800

    Bug 1 Replacing search function to avoid -1 subsitute and adding in price accounting for MaxPrice

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -22,26 +22,47 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String subCategory)
     {
         List<Product> products = new ArrayList<>();
+StringBuilder sql = new StringBuilder( "SELECT * FROM products where 1=1");
 
-        String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') ";
+        if (categoryId != null){
+            sql.append(" AND category_id = ?" );}
 
-        categoryId = categoryId == null ? -1 : categoryId;
-        minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
-        maxPrice = maxPrice == null ? new BigDecimal("-1") : maxPrice;
-        subCategory = subCategory == null ? "" : subCategory;
+        if (minPrice != null){
+            sql.append(" AND price >= ?  ");}
+
+        if (maxPrice != null){
+            sql.append(" AND price <= ? ");}
+
+        if (subCategory  != null){
+            sql.append(" AND subcategory = ? ");
+        }
+
+//       categoryId = categoryId == null ? -1 : categoryId;
+//        minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
+//        maxPrice = maxPrice == null ? new BigDecimal("-1") : maxPrice;
+//        subCategory = subCategory == null ? "" : subCategory;
 
         try (Connection connection = getConnection())
         {
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, categoryId);
-            statement.setInt(2, categoryId);
-            statement.setBigDecimal(3, minPrice);
-            statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
+
+            PreparedStatement statement = connection.prepareStatement(sql.toString());
+            int paramIndex = 1;
+
+            if (categoryId != null) {
+                statement.setInt(paramIndex++, categoryId);
+            }
+
+            if (minPrice != null) {
+                statement.setBigDecimal(paramIndex++, minPrice);
+            }
+
+            if (maxPrice != null) {
+                statement.setBigDecimal(paramIndex++, maxPrice);
+            }
+
+            if (subCategory != null) {
+                statement.setString(paramIndex++, subCategory);
+            }
 
             ResultSet row = statement.executeQuery();
 
